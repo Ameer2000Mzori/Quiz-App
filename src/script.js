b47a1abf4fdb9f57ab2a227702a075cba01fd421
Ameer <ameerapex496@gmail.com>
2023-12-13 18:45:26 +0100

    selected elements needed and added comments

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-console.log("hallo world");
+// selecting our elements
+var quastionText = document.getElementsByClassName("quastion-Text")[0];
+var aText = document.getElementsByClassName("a_Text")[0];
+var inputA = document.getElementById("A");
+var bText = document.getElementsByClassName("b_Text")[0];
+var inputB = document.getElementById("B");
+var cText = document.getElementsByClassName("c_Text")[0];
+var inputC = document.getElementById("C");
+var dText = document.getElementsByClassName("d_Text")[0];
+var inputD = document.getElementById("D");
+var submitBtn = document.getElementsByClassName("submit-Btn")[0];
+// our gelobal variables
+// our object Quastions
+// our functions
+// our event linsters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,19 @@
-console.log("hallo world");
+// selecting our elements
+const quastionText = document.getElementsByClassName("quastion-Text")[0];
+const aText = document.getElementsByClassName("a_Text")[0];
+const inputA = document.getElementById("A");
+const bText = document.getElementsByClassName("b_Text")[0];
+const inputB = document.getElementById("B");
+const cText = document.getElementsByClassName("c_Text")[0];
+const inputC = document.getElementById("C");
+const dText = document.getElementsByClassName("d_Text")[0];
+const inputD = document.getElementById("D");
+const submitBtn = document.getElementsByClassName("submit-Btn")[0];
+
+// our gelobal variables
+
+// our object Quastions
+
+// our functions
+
+// our event linsters
