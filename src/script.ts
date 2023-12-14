23dfd2144cd4b292b8e7ea77efcd6ed1e6d2a14d
Ameer <ameerapex496@gmail.com>
2023-12-14 06:22:59 +0100

    small change in selecting elements

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,11 @@
 // selecting our elements
 const quastionText = document.getElementsByClassName("quastion-Text")[0];
 const aText = document.getElementsByClassName("a_Text")[0];
-const inputA = document.getElementById("A");
 const bText = document.getElementsByClassName("b_Text")[0];
-const inputB = document.getElementById("B");
 const cText = document.getElementsByClassName("c_Text")[0];
-const inputC = document.getElementById("C");
 const dText = document.getElementsByClassName("d_Text")[0];
-const inputD = document.getElementById("D");
 const submitBtn = document.getElementsByClassName("submit-Btn")[0];
-
+const answersEl = document.querySelectorAll(".answer");
 // our gelobal variables
 
 // our object Quastions
